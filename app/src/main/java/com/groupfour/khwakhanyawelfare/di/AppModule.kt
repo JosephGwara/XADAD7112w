ce19e64a391fcbe4055b2edc4d8ae66e0423284c
Joseph Gwara <54535857+JosephGwara@users.noreply.github.com>
2024-09-02 22:59:24 +0200

    Added MyApplication class and AppModule for di

---
+++ app/src/main/java/com/groupfour/khwakhanyawelfare/di/AppModule.kt
@@ -0,0 +1,12 @@
+package com.groupfour.khwakhanyawelfare.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+}
