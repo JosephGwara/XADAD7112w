ac6c6ab43d365bfa7e86c7b6150716945e8fb1d7
Joseph Gwara <54535857+JosephGwara@users.noreply.github.com>
2024-09-24 13:49:06 +0200

    Added Validation to registration screen and progress indicator

--- app/src/main/java/com/groupfour/khwakhanyawelfare/presentation/auth/registration/RegistrationFragment.kt
+++ app/src/main/java/com/groupfour/khwakhanyawelfare/presentation/auth/registration/RegistrationFragment.kt
@@ -2,14 +2,18 @@ package com.groupfour.khwakhanyawelfare.presentation.auth.registration
 
 import androidx.fragment.app.viewModels
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.groupfour.khwakhanyawelfare.R
 import com.groupfour.khwakhanyawelfare.databinding.FragmentRegistrationBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.regex.Pattern
 
 @AndroidEntryPoint
 class RegistrationFragment : Fragment() {
@@ -27,19 +31,30 @@ class RegistrationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initListeners()
+        initObservers()
     }
 
-
+private fun initObservers(){
+    viewModel.registrationError.observe(viewLifecycleOwner){ error ->
+        binding.progressBar.isVisible = false
+        Snackbar.make(binding.root,error,Snackbar.LENGTH_SHORT).show()
+    }
+    viewModel.registrationSuccessful.observe(viewLifecycleOwner){ successful->
+       binding.progressBar.isVisible = false
+        if (successful)Snackbar.make(binding.root,"RegistrationSuccessful",Snackbar.LENGTH_SHORT).show()
+    }
+}
     private fun initListeners(){
        binding.apply {
            registerBtn.setOnClickListener {
-           validateUsername()
 
-               viewModel.validateUserDetails("jgwarix@gmail.com","Gwarix123456#","Gwarix23456#")
+              if (validateUsername() &&  validateEmail() && validateFirstPassword() && validateSecondPassword() && checkPasswordsMatch()){
+                  progressBar.isVisible = true
+                  viewModel.registerUser(binding.emailEditText.text.toString(),binding.passwordEditText.text.toString())
+              }
            }
          loginTxt.setOnClickListener {
                findNavController().navigate(R.id.action_registrationFragment_to_signInFragment)
-
            }
 
        }
@@ -47,14 +62,80 @@ class RegistrationFragment : Fragment() {
 
     private fun validateUsername():Boolean{
         val username = binding.usernameEditText.text.toString()
+        val pattern = "^[A-Za-z]\\w{5,29}$"
         if (username.isEmpty() || username.isBlank() ){
-            //TODO Show Error to User that username is invalid
+           binding.usernameEditText.error = "Please enter a username"
+            return false
+        }
+        else if(!Pattern.compile(pattern).matcher(username).matches()) {
+            val errorText = when {
+               !username.matches(Regex("^[A-Za-z].*")) -> "Username must start with a letter."
+                username.length !in 6..30 -> "Username must be between 6 and 30 characters long."
+                !username.matches(Regex("^[A-Za-z]\\\\w{5,29}\$")) -> "Username can only contain letters, digits, and underscores."
+                else -> ""
+            }
+            binding.usernameEditText.error = errorText
             return false
         }
         else{
-            viewModel.validateUsername(username)
             return true
         }
+    }
+    private fun validateEmail():Boolean{
+        val email = binding.emailEditText.text.toString()
+        val pattern = Patterns.EMAIL_ADDRESS
+        if (email.isEmpty() || email.isBlank()){
+            binding.emailEditText.error = "Please enter an email"
+            return false
+        }else if (!pattern.matcher(email).matches()){
+            binding.emailEditText.error = "Please enter a valid email"
+            return false
+        }
+        else{
+            return true
+        }
+    }
+    private fun validateFirstPassword():Boolean{
+        val firstPassword = binding.passwordEditText.text.toString()
+        if (firstPassword.isEmpty() || firstPassword.isEmpty()){
+            binding.passwordEditText.error = "Please enter a password"
+            return false
+        }
+        else if (!validatePasswordPattern(firstPassword)){
+            val errorText = when {
+                !firstPassword.contains(Regex(".*[A-Z].*")) -> "Password must contain one capital letter"
+                !firstPassword.contains(Regex(".*[a-z].*")) -> "Password must contain one lowercase letter"
+                !firstPassword.contains(Regex(".*[0-9].*")) -> "Password must contain one digit"
+                !firstPassword.contains(Regex(".*[@#\$%^&+=].*")) -> "Password must contain one special character"
+                !firstPassword.contains(Regex("\\\\s")) -> "Password must contain one special character"
+                firstPassword.length < 6 -> "Password must contain at least 6 characters "
+                else -> ""
+            }
+            binding.passwordEditText.error = errorText
+            return false
+        }
+        else{
+            return true
+        }
+    }
+
+    private fun validateSecondPassword():Boolean{
+        val secondPassword = binding.passwordConfirmEditText.text.toString()
+        if (secondPassword.isEmpty() || secondPassword.isEmpty()){
+            binding.passwordConfirmEditText.error = "Please confirm your password"
+            return false
+        }else{
+            return true
+        }
+    }
+    private fun checkPasswordsMatch():Boolean{
+        return binding.passwordEditText.text.toString() == binding.passwordConfirmEditText.text.toString()
+    }
+
+
+    private fun validatePasswordPattern(password: String):Boolean{
+        val pattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%^&+=])(?=\\S+\$).{6,}\$"
+        return Pattern.compile(pattern).matcher(password).matches()
     }
 
 
--- app/src/main/java/com/groupfour/khwakhanyawelfare/presentation/auth/registration/RegistrationViewModel.kt
+++ app/src/main/java/com/groupfour/khwakhanyawelfare/presentation/auth/registration/RegistrationViewModel.kt
@@ -1,11 +1,15 @@
 package com.groupfour.khwakhanyawelfare.presentation.auth.registration
 
 import android.util.Patterns
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import timber.log.Timber
 import java.util.regex.Pattern
 import javax.inject.Inject
@@ -13,49 +17,38 @@ import javax.inject.Inject
 @HiltViewModel
 class RegistrationViewModel @Inject constructor(private val firebaseAuth: FirebaseAuth): ViewModel() {
 
-    val TAG = this.javaClass.name
+    private val TAG = this.javaClass.name
 
-    fun validateUserDetails(email:String,firstPassword:String,secondPassword:String){
-        if (validateEmailAddress(email) && validatePasswords(firstPassword,secondPassword)){
-            registerUser(email,firstPassword)
-        }
-    }
+    private var _registrationError = MutableLiveData<String>()
+    val registrationError:LiveData<String> get() = _registrationError
 
-    private fun validateEmailAddress(email:String):Boolean{
-        val pattern = Patterns.EMAIL_ADDRESS
-        return pattern.matcher(email).matches()
-    }
-    private fun validatePasswords(firstPassword:String,secondPassword:String):Boolean{
-        return validatePasswordPattern(firstPassword) && firstPassword == secondPassword
-    }
-   fun validateUsername(username:String):Boolean{
-       val pattern = "^[A-Za-z]\\w{5,29}$"
-       return Pattern.compile(pattern).matcher(username).matches()
-    }
-   private fun validatePasswordPattern(password: String):Boolean{
-        val pattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%^&+=])(?=\\\\S+\$).{6,}\$"
-        return Pattern.compile(pattern).matcher(password).matches()
-    }
+    private var _registrationSuccessful = MutableLiveData<Boolean>()
+    val registrationSuccessful:LiveData<Boolean> get() = _registrationSuccessful
 
-
- private fun registerUser(email:String,password:String) = viewModelScope.launch {
+ fun registerUser(email:String,password:String) = viewModelScope.launch {
             try {
                 firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+
                     if (task.isSuccessful){
                         Timber.tag(TAG).d("User Registered successfully")
+                        _registrationSuccessful.postValue(true)
+
+                    }else if(task.exception?.message?.contains("The email address is already in use by another account")== true){
+                        Timber.tag(TAG).d("Failed to signUp, email already in use")
+                        viewModelScope.launch {
+                            withContext(Dispatchers.Main){
+                                _registrationError.postValue("Failed to Register, email already in use")
+                            }
+                        }
 
                     }else{
-                        Timber.tag(TAG).d("Failed to register User")
+                        Timber.tag(TAG).d("Failed to Register")
                     }
                 }
             }
         catch (e:Exception){
             Timber.e(e.message)
+            _registrationError.postValue(e.message)
         }
-
-
-
         }
-
-
 }
